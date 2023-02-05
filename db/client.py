419f23a950f5715652c6cbcdafbf9392fa60df77
kenny robert <skenrobert@gmail.com>
2023-02-05 17:18:57 -0500

    update

--- db/client.py
+++ db/client.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 
 # db_client = MongoClient() # local at the code
 # db_client = MongoClient().local # local here and not the code
-db_client = MongoClient("mongodb+srv://toor:3dNoI3hGvPR46FEt@cluster0.8jxxjrk.mongodb.net/?retryWrites=true&w=majority").test # test name data base
+db_client = MongoClient("mongodb+srv://toor:<password>@cluster0.8jxxjrk.mongodb.net/?retryWrites=true&w=majority").test # test name data base
 # mongodb://localhost:27017 #default address (local mongoDB extension vcode)
 
 
